e22fa09772c5b3e694f555038347dedfe926bd69
Zihao Zhang <zihaozhang@yahoo-inc.com>
2016-06-02 15:43:59 -0700

    Fk codepad

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const router  = express.Router();
 const request = require('request');
+const cheerio = require('cheerio');
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  res.send('respond with a resource');
+router.all('/', (req, res) => {
+  res.send('ok');
 });
 
-router.post('/codepad', (req, res) => {
-  request.post('http://codepad.org', { form: req.body },
+router.all('/code', (req, res) => {
+  request.post('http://codepad.org', { form: {
+    lang: req.body.lang || req.query.lang || 'Python',
+    code: req.body.code || req.query.code,
+    run: 'True',
+    submit: 'Submit'
+  } },
     (error, response) => {
       const redirect = response.headers.location;
-      console.log(redirect);
-      request(redirect).pipe(res);
+
+      request(redirect, (rerr, rres, body) => {
+        const $ = cheerio.load(body);
+        const output = $($('a[name=output]').next().find('pre')[1]).text();
+        res.json({ output, url: redirect });
+      });
     });
 });
 
