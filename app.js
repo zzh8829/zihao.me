d2f59b7144d6eef23b9265e629e8b225b29d33b5
Zihao Zhang <zihaozhang@yahoo-inc.com>
2016-06-07 13:04:05 -0700

    Improve styling

--- app.js
+++ app.js
@@ -3,35 +3,27 @@ global.Promise = require('bluebird');
 const express        = require('express');
 const bodyParser     = require('body-parser');
 const cookieParser   = require('cookie-parser');
-const errorHandler   = require('errorhandler');
-const methodOverride = require('method-override');
-const debug          = require('debug')('zihao.me:server');
 const path           = require('path');
-const logger         = require('morgan');
-const cors           = require('cors');
 
 const app    = express();
 const server = require('http').createServer(app);
 const io     = require('socket.io').listen(server);
 const port   = process.env.PORT || 3000;
 
-const routes = require('./routes/index');
-const api    = require('./routes/api');
-
 app.set('port', port);
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
-app.disable('x-powered-by');
+app.enable('trust proxy');
+app.disable('X-Powered-By');
 
-app.use(cors());
-app.use(logger('dev'));
-app.use(methodOverride());
-app.use(errorHandler());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const routes = require('./src/routes/index');
+const api    = require('./src/routes/api');
 
 app.use('/', routes);
 app.use('/api', api);
@@ -43,69 +35,16 @@ app.use((req, res, next) => {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use((err, req, res) => {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res) => {
+// render error page
+app.use((err, req, res, next) => {
+  const error = err;
+  if (app.get('env') !== 'development') {
+    error.stack = 'Sorry...';
+  }
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.render('error', { error });
+  next(err);
 });
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
 
 // WebSocket Stuff
 
@@ -169,3 +108,5 @@ function generateHeight(width, height) {
   return data;
 }
 /*eslint-enable */
+
+server.listen(port);
